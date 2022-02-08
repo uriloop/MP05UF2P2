422512ec5dfae1cb2b5106f04252b9723bfee789
uriloop <orlopez@elpuig.xeill.net>
2022-02-08 19:10:45 +0100

    commit

---
+++ src/test/java/ex2/HashTableTest.java
@@ -0,0 +1,31 @@
+package ex2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testToString() {
+
+
+    }
+}
